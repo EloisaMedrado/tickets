1f4159837092eeaddfdecd27498c5b8c462d2706
Eloisa Teixeira Medrado <eloisamedrado_11@hotmail.com>
2018-09-07 17:45:18 -0300

    Commit inicial - pacote de conexão.

---
+++ conexao/conexao.php
@@ -0,0 +1,25 @@
+<?php
+
+    require_once 'config.php';
+
+    class Conexao{
+
+        private static $mongo;
+
+        public static function getInstance(){
+            if(!isset(self::$mongo)){
+                try {
+                    self::$mongo = new MongoDB\Driver\Manager('mongodb://' . DB_HOST . ':' . DB_PORT . '/' . DB_NAME);     
+                } catch (MongoDB\Driver\Exception\Exception $e) {
+                    $filename = basename(__FILE__);
+                    echo "Erro no arquivo", $filename, "\n";
+                    echo "Exception:", $e->getMessage(), "\n";
+                    echo "Arquivo:", $e->getFile(), "\n";
+                    echo "Linha:", $e->getLine(), "\n";    
+                }
+            }    
+
+            return self::$mongo;
+        }
+    }
+?>
---
+++ conexao/config.php
@@ -0,0 +1,10 @@
+<?php
+	
+	define('DB_NAME','projeto');
+	define('DB_HOST', 'localhost');
+	define('DB_PORT', '27017');
+	define('DB_PASS','');
+	define('DB_USER','');
+
+?>
+
