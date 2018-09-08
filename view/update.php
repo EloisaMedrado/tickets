2b26806ce9e34714228e997faddfab344fa26ef4
Eloisa Teixeira Medrado <eloisamedrado_11@hotmail.com>
2018-09-07 21:13:43 -0300

    Criando updata view.

---
+++ view/update.php
@@ -0,0 +1,19 @@
+<?php
+    //Headers
+    header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/json; charset=UTF-8");
+    header("Access-Control-Allow-Methods: POST");
+    header("Access-Control-Max-Age: 3600");
+    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+    // Includes like imports
+    include_once '../control/ticketControl.php';
+
+    //Pega data do POST
+    $data = file_get_contents("php://input");
+
+    $ticketControl = new TicketControl();
+    $ticketControl->update($data);
+
+    // return ($ticket->update($obj));
+?>
