3676a8ecd95daec51e3d4864f154b586321ac195
Eloisa Teixeira Medrado <eloisamedrado_11@hotmail.com>
2018-09-08 16:17:17 -0300

    Criando função que retorna objeto de paginação.

--- control/ticketControl.php
+++ control/ticketControl.php
@@ -27,31 +27,43 @@
             return $retorno;
         }
 
+        function findByAndOrderPaginationObject($filterPriority, $filterStartDt, $filterEndDt, $order, $page, $pageSize) {
+
+            $paging = array();
+            $pages_arr = array();
+            $paging['paging'] = array();
+
+            $ticketRepository = new TicketRepository($this->mongo);
+            $qtTotalRows = $ticketRepository->countByDateCreateBetweenAndPriority($filterPriority, $filterStartDt, $filterEndDt, $order, $page, $pageSize);
+            $qtTotalPages = ceil($qtTotalRows/$pageSize);          
+
+            for ($i = 1; $i <= $qtTotalPages; $i++) {
+                if($i == $page){
+                    $paging['tickets'] = $this->findByAndOrder($filterPriority, $filterStartDt, $filterEndDt, $order, $page, $pageSize);
+                }
+                $urlPage = TicketUtils::getUrlPagination($filterPriority, $filterStartDt, $filterEndDt, $order, $i, $pageSize);
+                
+                if($i == $qtTotalPages) {
+                    $paging['paging']['last'] = $urlPage;
+                } else {
+                    $pages_arr = TicketUtils::getArrayPages($i, $urlPage, $i == $page);
+                }
+            }
+            $paging['paging']['pages'] = $pages_arr['pages'];
+
+            echo json_encode($paging);
+            return json_encode($paging);
+        }
+
         function findByAndOrder($filterPriority, $filterStartDt, $filterEndDt, $order, $page, $pageSize) {
 
             $ticketRepository = new TicketRepository($this->mongo);
 
             $rows = $ticketRepository->findByDateCreateBetweenAndPriorityAndOrder($filterPriority, $filterStartDt, $filterEndDt, $order, $page, $pageSize);
-            
-            
-            
             $tickets_arr = TicketUtils::getTicketsArray($rows);
 
-            $quantidadeRegistros = count($tickets_arr);
-            $pageSize =  is_null($pageSize) ? 3 : $pageSize;
-            $divisao = $quantidadeRegistros / $pageSize;
-            echo $quantidadeRegistros . ' - ' . $pageSize . ' - ' . $divisao;
-            $qtPages = ceil($divisao);
-            echo $qtPages; die();
-
-
-            //Retorna resultados da pag 1
-            if(!$tickets_arr) {
-                $rows = $ticketRepository->findByDateCreateBetweenAndPriorityAndOrder($filterPriority, $filterStartDt, $filterEndDt, $order, 1, 10);
-                $tickets_arr = TicketUtils::getTicketsArray($rows);
-            }
-
-            echo json_encode($tickets_arr);
+            // echo json_encode($tickets_arr);
+            return $tickets_arr;
         }
 
         function findAll() {
