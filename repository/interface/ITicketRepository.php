aeace5311801da9425766c37720f8e554eb51f31
Eloisa Teixeira Medrado <eloisamedrado_11@hotmail.com>
2018-09-10 00:02:10 -0300

    Adicionando função na interface.

--- repository/interface/ITicketRepository.php
+++ repository/interface/ITicketRepository.php
@@ -3,6 +3,8 @@
 interface ITicketRepository {
 
     function findAll();
+	
+	function countByDateCreateBetweenAndPriority($filterPriority, $filterStartDt, $filterEndDt, $order, $page, $pageSize)
 
     function findByDateCreateBetweenAndPriorityAndOrder($filterPriority, $filterStartDt, $filterEndDt, $order, $page, $pageSize);
 
